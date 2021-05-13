6cb215b189ede13c8812a24c28984577be2cec0c
Juan Camejo Alarcón <juancamejo93@gmail.com>
2021-05-13 19:22:58 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<div class="style-guide">
+		<div class="style-guide__container">
+			<div class="style-guide__row border-bottom">
+				<div class="style-guide__logos">
+					<div class="style-guide__logos__container">
+						<div class="style-guide__logos__first">
+							<span>PRIMARY LOGO</span>
+							<img src="https://juancamejoalarcon.s3.eu-west-3.amazonaws.com/logo-v1.svg" alt="logo">
+						</div>
+						<div class="style-guide__logos__second">
+							<span>SECONDARY LOGO</span>
+							<img src="https://juancamejoalarcon.s3.eu-west-3.amazonaws.com/logo-alternate-v1.svg" alt="logo2">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- <div class="logo-row">
+		<div class="logo-row__column">
+			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Amazon_Web_Services_Logo.svg/512px-Amazon_Web_Services_Logo.svg.png" alt="logo">
+		</div>
+		<div class="logo-row__column">
+			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/AmazonWebservices_Logo.svg/580px-AmazonWebservices_Logo.svg.png" alt="logo2">
+		</div>
+	</div> -->
+</main>
+
+<style>
+</style>
